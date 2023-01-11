9eed240c84c7fdce451802c9f090e4eb535a891b
ParkHyeJi <phj9908@gmail.com>
2023-01-11 14:48:57 +0900

    검색 끝

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -68,7 +68,7 @@ function search(){
                         '<div class="mainCnt" style="background: #fef1c6;border-bottom: 20px solid #f9fcf3;">' +
                         '<div style="display: grid;padding: 30px;padding-bottom: 20px;grid-template-columns: 300px 1fr;">' +
                         '<div id="img_div2" style="margin-right: 50px;margin-left: 10px;">' +
-                        '<img  id = "strImg" src=' + res.searchListStr[index].str_img + 'onError="this.src=\'https://post-phinf.pstatic.net/MjAyMDA0MjlfNjIg/MDAxNTg4MTQxNjU3NzQz.ZDWTjIfuekjZLxo3CHMoKl6D5yyhJaeiMo0Cb_x_JRcg.Pj0UivY3zH6VL1Z_tg9brZxQ78_kwJez5KH_IBt3gdAg.PNG/%EC%9D%8C%EC%8B%9D_%EC%9D%BC%EB%9F%AC%EC%8A%A4%ED%8A%B8_%EB%8F%84%EB%84%9B_%EA%B7%B8%EB%A6%BC%EA%B7%B8%EB%A6%AC%EA%B8%B0.png?type=w1200\';"/></div>' +
+                        '<img  referrerpolicy="no-referrer" id = "strImg" src="/img/' + res.searchListStr[index].str_img + '" onError="this.src=\'https://post-phinf.pstatic.net/MjAyMDA0MjlfNjIg/MDAxNTg4MTQxNjU3NzQz.ZDWTjIfuekjZLxo3CHMoKl6D5yyhJaeiMo0Cb_x_JRcg.Pj0UivY3zH6VL1Z_tg9brZxQ78_kwJez5KH_IBt3gdAg.PNG/%EC%9D%8C%EC%8B%9D_%EC%9D%BC%EB%9F%AC%EC%8A%A4%ED%8A%B8_%EB%8F%84%EB%84%9B_%EA%B7%B8%EB%A6%BC%EA%B7%B8%EB%A6%AC%EA%B8%B0.png?type=w1200\';"/></div>' +
                         ' <div style="display: grid; grid-template-rows: 0.2fr 0.1fr 1fr 0.1fr;">' +
 
                         '<div style="display: flex;justify-content: space-between;align-items: stretch;">' +
